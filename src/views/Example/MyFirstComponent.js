1cc519997a47f3289ece651d4fe2704577aa5f90
khanhdp95 <phuckhanh22@gmail.com>
2021-10-05 18:07:55 +0700

    finish 5, test state and onChange event listener

--- src/views/Example/MyFirstComponent.js
+++ src/views/Example/MyFirstComponent.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 export default class MyFirstComponent extends Component {
+  state = {
+    name: 'Kido',
+    job: 'dev',
+  };
+  handleOnChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
   render() {
     return (
-      <div>
-        <h1>hihi hehe</h1>
-      </div>
+      <>
+        <div>
+          <input type="text" onChange={(e) => this.handleOnChange(e)} />
+          <h1>Tên tui là {this.state.name}</h1>
+        </div>
+        <div>
+          <h1>Tui chuẩn bị làm {this.state.job}</h1>
+        </div>
+      </>
     );
   }
 }
