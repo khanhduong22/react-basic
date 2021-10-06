0e2b466e7d373710fa51ac36f6108b277808ebf7
khanhdp95 <phuckhanh22@gmail.com>
2021-10-06 10:33:27 +0700

    stateless vs statefull and class conponent (rcc) vs function component (rfc)

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-export default class ChildComponent extends Component {
-  render() {
-    return (
-      <div>
-        <div> This is full name from props: {this.props.fname}</div>
-      </div>
-    );
-  }
-}
+// export default class ChildComponent extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <div> This is first name from props: {this.props.fname}</div>
+//       </div>
+//     );
+//   }
+// }
+
+import React from 'react';
+
+const Childcomponent = (props) => {
+  let { fname } = props;
+  return (
+    <div>
+      <div> This is first name from props: {fname}</div>
+    </div>
+  );
+};
+
+export default Childcomponent;
