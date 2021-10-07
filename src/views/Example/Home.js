ad54489b054929f180baa4848e6ba3e5bccac07e
khanhdp95 <phuckhanh22@gmail.com>
2021-10-07 14:49:36 +0700

    learn about Higher Order Component

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import Color from "../HOC/Color"
 
-export default class Home extends Component {
+// export default class Color(Home) extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is homepage</h1>
+//       </div>
+//     );
+//   }
+// }
+import React, { Component } from 'react';
+import Color from '../HOC/Color';
+
+class Home extends Component {
   render() {
     return (
       <div>
-        <h1>This is homepage</h1>
+        <h1>This is homepage with color random when reload</h1>
       </div>
     );
   }
 }
+
+export default Color(Home);
