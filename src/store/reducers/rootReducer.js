2c6b52bc3770a12e4a18cf899adcdf59c4042502
khanhdp95 <phuckhanh22@gmail.com>
2021-10-08 14:00:55 +0700

    config redux basic

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  users: [
+    { id: 1, name: 'Kido' },
+    { id: 2, name: 'Hero' },
+  ],
+};
+
+const rootReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default rootReducer;
